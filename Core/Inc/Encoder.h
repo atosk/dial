c5cdec52411ed0ed9e56e666b128e0063c89e16c
jason <jason>
2021-04-25 18:53:58 -0700

    Encoder class is useful
    Reads location on startup and sets that to zero. All subsequent location reads are corrected by that factor. 
    
    GetLocation returns the dial number in range 0.0 to 100.0
    
    Hysteresis is currently commented out. I may play with that again later.

--- Core/Inc/Encoder.h
+++ Core/Inc/Encoder.h
@@ -11,14 +11,16 @@
 #include "main.h"
 
 // AS5600 Hall Effect sensor constants
-#define AS5600_ADDR 0x36 << 1
-#define AS5600_REG_ANGLE_H 0x0E
-#define AS5600_REG_ANGLE_L 0x0F
-#define AS5600_REG_STATUS 0x0B
-#define AS5600_REG_CONF_H 0x07
-#define AS5600_REG_CONF_L 0x08
-#define AS5600_REG_ZPOS 0x01
-#define AS5600_REG_MPOS 0x03
+#define AS5600_ADDR            0x36 << 1
+#define AS5600_REG_ANGLE_H     0x0E
+#define AS5600_REG_ANGLE_L     0x0F
+#define AS5600_REG_ANGLE_RAW_H 0x0C
+#define AS5600_REG_ANGLE_RAW_L 0x0D
+#define AS5600_REG_STATUS      0x0B
+#define AS5600_REG_CONF_H      0x07
+#define AS5600_REG_CONF_L      0x08
+#define AS5600_REG_ZPOS        0x01
+#define AS5600_REG_MPOS        0x03
 
 #define AS5600_HYSTERESIS_1LSB 4
 
@@ -37,18 +39,19 @@ namespace std {
 
 class Encoder {
 private:
-   I2C_HandleTypeDef m_hi2cx;
+   I2C_HandleTypeDef *m_hi2cx;
    uint8_t m_i2c_receive_buf[16] = {0};
    char m_msg[100] = {0};
-   int m_angle = 0;
+   double m_angle = 0;
+   int m_starting_angle = 0;
    enum I2C_Status m_status = Ready;
    enum I2C_Status m_mag_status = Magnet_Missing;
 
 
 public:
-   Encoder(I2C_HandleTypeDef, int);
+   Encoder(I2C_HandleTypeDef*, int);
    enum I2C_Status GetStatus();
-   int GetAngle();
+   float GetLocation();
    ~Encoder();
 };
 
